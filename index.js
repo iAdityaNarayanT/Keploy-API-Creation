d07f2be8291633a69de42a5eee620603aa130bf1
Aditya Narayan Tiwari <98219031+iAdityaNarayanT@users.noreply.github.com>
2023-02-01 16:48:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors())
+
+const port = process.env.PORT || 3000;
+
+const apiData = require("./data.json")
+app.get("/", (req, res) => {
+    res.send("Hello I am live")
+});
+
+app.get("/service", (req, res) => {
+    res.send(apiData)
+})
+app.listen(port, () => {
+    console.log("I am Live again");
+});
